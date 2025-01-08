7f29b94db0d34c9d5a8eb71d0d913e1c3b676e1b
quangpao <ddquangbao@gmail.com>
2025-01-08 23:06:10 +0700

    :construction: Add HelloWorld component, index page, and robots.txt for admin interface

---
+++ src/admin/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <button @click="increment">Clicked {{  count }} times</button>
+    </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+    setup() {
+        const count = ref(1);
+        const message = 'Hello from Vue!';
+
+        const increment = () => {
+            count.value++;
+        };
+
+        return { count, message, increment }
+    },
+};
+</script>
+
+<style>
+h1 {
+    color: #42b983;
+}
+</style>
